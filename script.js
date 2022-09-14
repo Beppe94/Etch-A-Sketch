fd0873ae5654453326afcffe7025e38d762668a6
Beppe94 <giuseppe.monorchio94@hotmail.it>
2022-09-14 21:58:39 +0200

    added functions to change colors and erase

--- script.js
+++ script.js
@@ -1,18 +1,68 @@
-const gridC = document.getElementById('gridContainer')
+const DEFAULT_COLOR = 'black'
+const DEFAULT_SIZE = 16
+const DEFAULT_MODE = 'color'
 
+let currentColor = DEFAULT_COLOR
+let currentSize = DEFAULT_SIZE
+let currentMode = DEFAULT_MODE
+
+const gridC = document.getElementById('gridContainer')
+const eraserBtn = document.getElementById('eraser')
+const clearGrid = document.getElementById('clearGrid')
+const rainbowBtn = document.getElementById('rainbow')
+const colorInput = document.getElementById('colorInput')
+const color = document.getElementById('color')
+
+eraserBtn.onclick = () => setMode('eraser')
+rainbowBtn.onclick = () => setMode('rainbow')
+color.onclick = () => setMode('color')
+colorInput.oninput = (e) => setColor(e.target.value)
+clearGrid.onclick = () => reloadGrid()
+
+function setMode(newMode) {
+    currentMode = newMode
+}
+
+function setColor(newColor) {
+    currentColor = newColor
+}
 
 function makeGrid(size) {
     gridC.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     gridC.style.gridTemplateRows = `repeat(${size}, 1fr)`
     
-    for(let i = 0; i < (size*size);i++) {
+    for(let i = 0; i < (size*size); i++) {
         const grid = document.createElement('div')
+        grid.addEventListener('mouseover', changeColor)
         gridC.appendChild(grid).className = 'grid-item';
     }
 }
 
-gridC.addEventListener('mouseover', function handleMouseOver(e) {
-    e.target.style.backgroundColor = 'red'
-})  
+function reloadGrid() {
+    clear()
+    makeGrid(currentSize)
+}
 
-makeGrid(16, 16)
+function clear() {
+    gridC.innerText = ''
+}
+
+function changeColor(e) {
+    if(e.type == 'mouseover' && !'mousedown') return
+    if(currentMode == 'rainbow') {
+        let randomR = Math.floor(Math.random() * 256)
+        let randomG = Math.floor(Math.random() * 256)
+        let randomB = Math.floor(Math.random() * 256)
+        e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if(currentMode == 'color') {
+        e.target.style.backgroundColor = currentColor
+    } else if(currentMode == 'eraser') {
+        e.target.style.backgroundColor = 'white'
+    }
+
+}
+
+
+window.onload = () => {
+    makeGrid(DEFAULT_SIZE)
+}
